5674e751294d5957d0344f2b48a7a01bb58d0824
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-04 17:25:43 +0800

    Add files via upload

--- pages/recordHand/xuetang/xuetang.js
+++ pages/recordHand/xuetang/xuetang.js
@@ -5,9 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
+    array: ['空腹', '早餐后', '午餐前', '午餐后', '晚餐前', '晚餐后', '睡前', '凌晨'],
+    objectArray: [
+      {
+        id: 0,
+        name: '空腹'
+      },
+      {
+        id: 1,
+        name: '早餐后'
+      },
+      {
+        id: 2,
+        name: '午餐前'
+      },
+      {
+        id: 3,
+        name: '午餐后'
+      },
+      {
+        id: 4,
+        name: '晚餐前'
+      },
+      {
+        id: 5,
+        name: '晚餐后'
+      },
+      {
+        id: 6,
+        name: '睡前'
+      },
+      {
+        id: 7,
+        name: '凌晨'
+      }
+    ],
+    index: 0,
 
   },
+  /**
+   * 日期控件
+   * @param {*} e 
+   */
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
 
+  /**
+   * 多项选择控件
+   * @param {*} e 
+   */
+    bindPickerChange: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        index: e.detail.value
+      })
+    },
   /**
    * 生命周期函数--监听页面加载
    */
