300af5f930b01ba84c5b8a28a8137714f66d42b2
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-09 21:29:49 +0800

    Add files via upload

--- config/api.js
+++ config/api.js
@@ -1,5 +1,13 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: pxf
+ * @Date: 2018-12-01 16:28:17
+ * @LastEditTime: 2018-12-08 16:27:43
+ * @LastEditors: 这个是与后台的链接接口部分，统一在这里进行处理
+ */
+
 const ApiRootUrl = 'http://127.0.0.1:8360/api/';
 
 module.exports = {
-    
+    SugarRecord: ApiRootUrl + 'sugar/post'
 }
