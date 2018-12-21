ca69371b8925f4bc48f35e0d3832716019b198f3
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-21 14:37:18 +0800

    Add files via upload

--- config/api.js
+++ config/api.js
@@ -2,7 +2,7 @@
  * @Description: 与后台连接的统一的API
  * @Author: pxf
  * @Date: 2018-12-01 16:28:17
- * @LastEditTime: 2018-12-18 17:50:40
+ * @LastEditTime: 2018-12-21 11:54:29
  * @LastEditors: Please set LastEditors
  */
 
@@ -10,5 +10,6 @@ const ApiRootUrl = 'http://127.0.0.1:8360/api/';
 
 module.exports = {
     SugarRecord: ApiRootUrl + 'sugar/sugarhand' ,   //文件名+action名称  手动记录血糖
-    SugarAsr: ApiRootUrl + 'asr/asrvoice' //语音记录血糖
+    SugarAsr: ApiRootUrl + 'asr/asrvoice', //语音记录血糖
+    PressureRecord: ApiRootUrl + 'bloodpressure/pressurehand'
 }
