11b3d2d6021f6f82388330a34412d6f03946e469
Taylorzun <33285361+TaylorZun@users.noreply.github.com>
2018-12-24 17:05:04 +0800

    Add files via upload

--- config/api.js
+++ config/api.js
@@ -2,11 +2,14 @@
  * @Description: 与后台连接的统一的API
  * @Author: pxf
  * @Date: 2018-12-01 16:28:17
- * @LastEditTime: 2018-12-23 15:50:55
+ * @LastEditTime: 2018-12-24 10:47:20
  * @LastEditors: Please set LastEditors
  */
 
+// const ApiRootUrl = 'http://localhost:8360/api/';
 const ApiRootUrl = 'http://127.0.0.1:8360/api/';
+//const ApiRootUrl = 'http://192.168.43.55:8360/api/'; //连接手机测试要用这个才能请求到数据
+
 
 module.exports = {
     SugarRecord: ApiRootUrl + 'sugar/sugarhand' ,   //文件名+action名称  手动记录血糖
